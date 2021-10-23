065ecbe7794a949895173e9ea8c8c3f7e9d2b111
André <afmendes@sga.pucminas.br>
2021-10-23 10:47:46 -0300

    Alteração produtos e cadastramento
    Alteração da funcionalidade de cadastramento de produtos e inclusão da funcionalidade cadastramento de estabelecimentos

---
+++ Codigo/Estabelecimentos/estabelecimentos.js
@@ -0,0 +1,82 @@
+// declara um conjunto inicial de estabelecimentos
+var db_estabelecimento_inicial = {
+    "data": [
+        {
+            "id": 1,
+            "nome": "Extra Hipermercados",
+            "regiao": "Centro"
+        },
+        {
+            "id": 2,
+            "nome": "Supermercado Dia",
+            "regiao": "Sul"
+        },
+        {
+            "id": 3,
+            "nome": "Supermercado Verdemar",
+            "regiao": "Sul"
+        },
+        {
+            "id": 5,
+            "nome": "Supermercado SuperNosso",
+            "regiao": "Sul"
+        },
+        {
+            "id": 6,
+            "nome": "Supermecado BH",
+            "regiao": "Oeste"
+        },
+     ]
+}
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var db = JSON.parse(localStorage.getItem('db_estabelecimento'));
+if (!db) {
+    db = db_estabelecimento_inicial
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertEstabelecimento(estabelecimento) {
+    // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+    let novoId = db.data[db.data.length - 1].id + 1;
+    let novoEstabelecimento = {
+        "id": novoId,
+        "nome": estabelecimento.nome,
+        "regiao": estabelecimento.regiao,
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novoEstabelecimento);
+    displayMessage("Estabelecimento inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_estabelecimento', JSON.stringify(db));
+}
+
+function updateEstabelecimento(id, estabelecimento) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    db.data[index].nome = estabelecimento.nome,
+    db.data[index].regiao = estabelecimento.regiao,
+
+    displayMessage("Estabelecimento alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_estabelecimento', JSON.stringify(db));
+}
+
+function deleteEstabelecimento(id) {    
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    displayMessage("Estabelecimento removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_estabelecimento', JSON.stringify(db));
+}
--- Codigo/Produtos/produtos.js
+++ Codigo/Produtos/produtos.js
@@ -4,92 +4,72 @@ var db_produtos_inicial = {
         {
             "id": 1,
             "nome": "Café Tradicional",
-            "marca": "3 Corações",
+            "fabricante": "3 Corações",
             "categoria": "Mercearia",
-            "valor": "R$ 7,39",
-            "estabelecimento": "Extra Hipermercados",
-            "regiao": "Centro"
+            "valor": "R$ 7,39"
         },
         {
             "id": 2,
             "nome": "Suco de Maracujá",
-            "marca": "Tial",
+            "fabricante": "Tial",
             "categoria": "Bebidas",
-            "valor": "R$ 5,39",
-            "estabelecimento": "Supermercado Dia",
-            "regiao": "Sul"
+            "valor": "R$ 5,39"
         },
         {
             "id": 3,
             "nome": "Leite Longa Vida Integral",
-            "marca": "Cotochés",
+            "fabricante": "Cotochés",
             "categoria": "Laticínios e Ovos",
-            "valor": "R$ 3,58",
-            "estabelecimento": "Supermercado Dia",
-            "regiao": "Sul"
+            "valor": "R$ 3,58"
         },
         {
             "id": 4,
             "nome": "Leite Longa Vida Integral",
-            "marca": "Cotochés",
+            "fabricante": "Cotochés",
             "categoria": "Laticínios e Ovos",
-            "valor": "R$ 4,29",
-            "estabelecimento": "Supermercado Verdemar",
-            "regiao": "Sul"
+            "valor": "R$ 4,29"
         },
         {
             "id": 5,
             "nome": "Requeijão Tradicional",
-            "marca": "Itambé",
+            "fabricante": "Itambé",
             "categoria": "Queijos e frios",
-            "valor": "R$ 9,49",
-            "estabelecimento": "Supermercado SuperNosso",
-            "regiao": "Sul"
+            "valor": "R$ 9,49"
         },
         {
             "id": 6,
             "nome": "Requeijão Tradicional",
-            "marca": "Itambé",
+            "fabricante": "Itambé",
             "categoria": "Queijos e frios",
-            "valor": "R$ 9,39",
-            "estabelecimento": "Supermercado Verdemar",
-            "regiao": "Sul"
+            "valor": "R$ 9,39"
         },
         {
             "id": 7,
             "nome": "Pernil Suíno - Kg",
-            "marca": "Supermercado BH",
+            "fabricante": "Supermercado BH",
             "categoria": "Açougue e peixaria",
-            "valor": "R$ 14,90",
-            "estabelecimento": "Supermecado BH",
-            "regiao": "Oeste"
+            "valor": "R$ 14,90"
         },
         {
             "id": 8,
             "nome": "Arroz Agulhinha - Tipo 1",
-            "marca": "Supermercado BH",
+            "fabricante": "Supermercado BH",
             "categoria": "Mercearia",
-            "valor": "R$ 16,98",
-            "estabelecimento": "Supermecado BH",
-            "regiao": "Oeste"
+            "valor": "R$ 16,98"
         },
         {
             "id": 9,
             "nome": "Leite Longa Vida Integral",
-            "marca": "Supermercado BH",
+            "fabricante": "Supermercado BH",
             "categoria": "Laticínios e Ovos",
-            "valor": "R$ 3,39",
-            "estabelecimento": "Supermecado BH",
-            "regiao": "Oeste"
+            "valor": "R$ 3,39"
         },
         {
             "id": 10,
             "nome": "Café Extra Forte",
-            "marca": "Pilão",
+            "fabricante": "Pilão",
             "categoria": "Mercearia",
-            "valor": "R$ 13,49",
-            "estabelecimento": "Hipermercado Extra",
-            "regiao": "Oeste"
+            "valor": "R$ 13,49"
         }
     ]
 }
@@ -111,12 +91,9 @@ function insertProduto(produto) {
     let novoProduto = {
         "id": novoId,
         "nome": produto.nome,
-        "marca": produto.marca,
+        "fabricante": produto.fabricante,
         "categoria": produto.categoria,
-        "valor": produto.valor,
-        "estabelecimento": produto.estabelecimento,
-        "regiao": produto.regiao,
-        "website": produto.website
+        "valor": produto.valor
     };
 
     // Insere o novo objeto no array
@@ -133,12 +110,9 @@ function updateProduto(id, produto) {
 
     // Altera os dados do objeto no array
     db.data[index].nome = produto.nome,
-    db.data[index].marca = produto.marca,
+    db.data[index].fabricante = produto.fabricante,
     db.data[index].categoria = produto.categoria,
-    db.data[index].valor = produto.valor,
-    db.data[index].estabelecimento = produto.estabelecimento,
-    db.data[index].regiao = produto.regiao,
-    db.data[index].website = produto.website
+    db.data[index].valor = produto.valor
 
     displayMessage("Produto alterado com sucesso");
 
