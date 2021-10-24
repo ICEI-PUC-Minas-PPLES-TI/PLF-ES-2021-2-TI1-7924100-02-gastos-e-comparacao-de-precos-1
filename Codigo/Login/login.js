f7a7987be3dbb3fe8360fef0f6f062d12641c8b5
Guilherme Dantas <guilherme.dantas1508@gmail.com>
2021-10-24 18:03:07 -0300

    Finalizaçao cadastro usuario, login e logout

--- Codigo/Login/login.js
+++ Codigo/Login/login.js
@@ -2,9 +2,7 @@ let db_users = {
   users: [],
 };
 
-let currentUser = {};
-
-//document.getElementById("form-login").addEventListener("submit", tryLogin);
+let current_user = {};
 
 function createUser(email, name, password, userType) {
   let newUser = {
@@ -33,57 +31,30 @@ function generateID() {
 // Código LoginApp utilizado como exemplo para alunos de primeiro período
 
 // Página inicial de Login
-const LOGIN_URL = "login.html";
+const LOGIN_URL = "index.html";
+const HOME_URL = "home.html"
 
 function startApp() {}
 
 // Inicializa o usuarioCorrente e banco de dados de usuários da aplicação de Login
-function initLoginApp() {
+function startApp() {
   // PARTE 1 - INICIALIZA USUARIOCORRENTE A PARTIR DE DADOS NO LOCAL STORAGE, CASO EXISTA
-  currentUserJSON = sessionStorage.getItem("currentUser");
-  if (usuarioCorrenteJSON) {
-    usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
+  currentUserJSON = sessionStorage.getItem("current_user");
+  if (currentUserJSON) {
+    current_user = JSON.parse(currentUserJSON);
   }
 
-  var usersJSON = localStorage.getItem("db_usuarios");
+  var usersJSON = localStorage.getItem("db_users");
   if (usersJSON) {
     db_users = JSON.stringify(usersJSON);
   }
 }
 
-// Verifica se o login do usuário está ok e, se positivo, direciona para a página inicial
-function loginUser(login, senha) {
-  // Verifica todos os itens do banco de dados de usuarios
-  // para localizar o usuário informado no formulario de login
-  for (var i = 0; i < db_usuarios.usuarios.length; i++) {
-    var usuario = db_usuarios.usuarios[i];
-
-    // Se encontrou login, carrega usuário corrente e salva no Session Storage
-    if (login == usuario.login && senha == usuario.senha) {
-      usuarioCorrente.id = usuario.id;
-      usuarioCorrente.login = usuario.login;
-      usuarioCorrente.email = usuario.email;
-      usuarioCorrente.nome = usuario.nome;
-
-      // Salva os dados do usuário corrente no Session Storage, mas antes converte para string
-      sessionStorage.setItem(
-        "usuarioCorrente",
-        JSON.stringify(usuarioCorrente)
-      );
-
-      // Retorna true para usuário encontrado
-      return true;
-    }
-  }
-
-  // Se chegou até aqui é por que não encontrou o usuário e retorna falso
-  return false;
-}
 
 // Apaga os dados do usuário corrente no sessionStorage
-function logoutUser() {
-  usuarioCorrente = {};
-  sessionStorage.setItem("usuarioCorrente", JSON.stringify(usuarioCorrente));
+function logout() {
+  user = {};
+  sessionStorage.setItem("usuarioCorrente", JSON.stringify(user));
   window.location = LOGIN_URL;
 }
 
@@ -101,7 +72,144 @@ function addUser(nome, login, senha, email) {
   localStorage.setItem("db_usuarios", JSON.stringify(db_usuarios));
 }
 
-function setUserPass() {}
+function verifyLogin() {
+  let emailInput = document.getElementById("email-login").value;
+  let passwordInput = document.getElementById("password-login").value;
+
+  db_users = JSON.parse(localStorage.getItem('db_users'))
+
+  if(db_users.users.length == 0){
+    alert("Não existe uma combinação de email e senha registrados! Verifique os dados ou se cadastre no site caso ainda não tenha feito seu registro!")
+    return;
+  }
+
+  for (let i = 0; i < db_users.users.length; i++) {
+    let registredUser = db_users.users[i];
+    if (
+      emailInput == registredUser.email &&
+      passwordInput == registredUser.password
+    ) {
+      sessionStorage.setItem("current_user", JSON.stringify(registredUser))
+      window.location = HOME_URL
+      break;
+    } else {
+      alert(
+        "Não existe uma combinação de email e senha registrados! Verifique os dados ou se cadastre no site caso ainda não tenha feito seu registro!"
+      );
+    }
+  }
+}
+
+function validateEmail() {
+  let email = document.getElementById("form-email").value;
+
+  if (!email) {
+    document.getElementById("form-email").style.borderColor = "red";
+    document.getElementById("form-email").style.backgroundColor =
+      "rgba(245,0,0, 0.2)";
+    document.getElementById("email-span").innerHTML = "O email é obrigatório!";
+    return false;
+  } else {
+    document.getElementById("form-email").style.borderColor = "green";
+    document.getElementById("form-email").style.backgroundColor =
+      "rgba(0,245,0, 0.2)";
+    document.getElementById("email-span").innerHTML = "";
+    return true;
+  }
+}
+
+function validateName() {
+  let name = document.getElementById("form-name").value;
+
+  if (!name) {
+    document.getElementById("form-name").style.borderColor = "red";
+    document.getElementById("form-name").style.backgroundColor =
+      "rgba(245,0,0, 0.2)";
+    document.getElementById("name-span").innerHTML = "O nome é obrigatório!";
+    return false;
+  } else {
+    document.getElementById("form-name").style.borderColor = "green";
+    document.getElementById("form-name").style.backgroundColor =
+      "rgba(0,245,0, 0.2)";
+    document.getElementById("name-span").innerHTML = "";
+    return true;
+  }
+}
+
+function validatePassword() {
+  let password = document.getElementById("form-password").value;
+  let rptPassowrd = document.getElementById("form-password-rpt").value;
+
+  let passwordSpan = document.getElementById("password-span");
+  let rptPasswordSpan = document.getElementById("password-rpt-span");
+
+  if (!password) {
+    document.getElementById("form-password").style.borderColor = "red";
+    document.getElementById("form-password").style.backgroundColor =
+      "rgba(245,0,0, 0.2)";
+    passwordSpan.innerHTML = "A senha é obrigatória";
+  } else {
+    document.getElementById("form-password").style.borderColor = "green";
+    document.getElementById("form-password").style.backgroundColor =
+      "rgba(0,245,0, 0.2)";
+    passwordSpan.innerHTML = "";
+  }
+
+  if (!rptPassowrd) {
+    document.getElementById("form-password-rpt").style.borderColor = "red";
+    document.getElementById("form-password-rpt").style.backgroundColor =
+      "rgba(245,0,0, 0.2)";
+    rptPasswordSpan.innerHTML = "A repetição da senha é obrigatória";
+  } else {
+    rptPasswordSpan.innerHTML = "";
+  }
+
+  if (password && rptPassowrd) {
+    if (password == rptPassowrd) {
+      document.getElementById("form-password-rpt").style.borderColor = "green";
+      document.getElementById("form-password-rpt").style.backgroundColor =
+        "rgba(0,245,0, 0.2)";
+      rptPasswordSpan.innerHTML = "";
+      return true;
+    } else {
+      rptPasswordSpan.innerHTML = "As senhas devem ser iguais";
+      return false;
+    }
+  }
+}
+
+function getNewUserData() {
+  preventDefault();
+  let emailValidation = validateEmail();
+  let nameValidation = validateName();
+  let passwordValidation = validatePassword();
+
+  let email = document.getElementById("form-email").value;
+  let name = document.getElementById("form-name").value;
+  let password = document.getElementById("form-password").value;
+
+  if (emailValidation && nameValidation && passwordValidation) {
+    closeModal();
+    createUser(email, name, password, null);
+
+    $("#modal-register").modal("hide");
+  }
+}
+
+function closeModal() {
+  var elementsInput = document.getElementsByClassName("form-control");
+  var elementsSpan = document.getElementsByClassName("error-span");
+
+  Array.prototype.forEach.call(elementsSpan, (e) => {
+    e.innerHTML = "";
+  });
+
+  Array.prototype.forEach.call(elementsInput, (e) => {
+    e.value = null;
+    e.style.backgroundColor = "white";
+    e.style.borderColor = "#ced4da";
+  });
+}
 
 // Inicializa as estruturas utilizadas pelo LoginApp
 startApp();
